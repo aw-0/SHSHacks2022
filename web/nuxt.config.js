4aaff0a524e23df10162c140bcaedaf23afde929
Andrew <43796708+aw-0@users.noreply.github.com>
2022-04-02 20:45:16 -0500

    :fire: Add Firebase

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -44,7 +44,23 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/firebase'
   ],
+
+  firebase: {
+    config: {
+      apiKey: "AIzaSyACZ8vwAaO4wW5vLMppVo9YyAB3RNDHG3E",
+      authDomain: "irc-tracker.firebaseapp.com",
+      projectId: "irc-tracker",
+      storageBucket: "irc-tracker.appspot.com",
+      messagingSenderId: "681178961887",
+      appId: "1:681178961887:web:36d8a7f786646b66f6566a"
+    },
+    services: {
+      auth: true,
+      firestore: true
+    }
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
